2a957dcde9e9e035c9ebdb83dbdf061449427062
Thaneshwar <Thaneshwar.js@gmail.com>
2024-06-12 00:48:19 +0530

    Call back-hell

---
+++ README.md
@@ -0,0 +1,13 @@
+                                                     call back -hell task
+
+*(using html,bootstrap,javascript)
+
+1. using HTML display the content and used "div tag and h1 tag"
+
+2. using boostarp style the bacground-color,text-color,fontsize ,flex
+
+3. javascript using this call back hell  Created the setTimeout function and using the queryselector give the innerText value to the h1 tag
+
+
+
+                               ..............Thank You..............
---
+++ js/script.js
@@ -0,0 +1,33 @@
+setTimeout(() => {
+  document.querySelector(".display").innerText = "10";
+setTimeout(() =>{
+    document.querySelector(".display").innerText = "9";
+    setTimeout(() =>{
+        document.querySelector(".display").innerText = "8";
+    setTimeout(() =>{
+        document.querySelector(".display").innerText = "7";
+        setTimeout(() =>{
+            document.querySelector(".display").innerText = "6";
+            setTimeout(() =>{
+                document.querySelector(".display").innerText = "5";
+                setTimeout(() =>{
+                    document.querySelector(".display").innerText = "4";
+                    setTimeout(()=>{
+                        document.querySelector(".display").innerText = "3";
+                        setTimeout(() =>{
+                            document.querySelector(".display").innerText = "2";
+                            setTimeout(()=>{
+                                document.querySelector(".display").innerText = "1";
+                                setTimeout(()=>{
+                                    document.querySelector(".display").innerText = "Happy Independence Day";
+                                })
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+    },1000);
+},1000);
+},1000);
